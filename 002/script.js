080e4f85dcf4f78186f2561df7e06e3394267668
tijnvlith <66628263+tijnvlith@users.noreply.github.com>
2020-07-06 22:26:22 +0200

    Add files via upload

---
+++ 002/script.js
@@ -0,0 +1,11 @@
+console.log("Hallo Winc Academy studenten")
+
+//dit is een grote som
+
+let multiplication = 1230941 * 1823794;
+
+console.log(multiplication)
+
+let devision = 637263 / 54;
+
+console.log(devision)
